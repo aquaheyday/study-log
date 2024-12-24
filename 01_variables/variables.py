8ae1de5eaf9f6530b5ad9375059414c04de6905b
김병준 <120731337+aquaheyday@users.noreply.github.com>
2024-12-24 15:13:34 +0900

    Create variables.py

---
+++ 01_variables/variables.py
@@ -0,0 +1,22 @@
+name = 'HongGilDong'
+age = 25
+is_men = True
+skills = ['HTML', 'CSS', 'JS', 'Python']
+print('Name:', name)
+print('Name length:', len(name))
+print('Age: ', age)
+print('Is men: ', is_men)
+print('Skills: ', skills)
+
+person_info = {
+    'name':'HongGilDong',
+    'age': 25,
+    'is_men': True,
+    'skills': ['HTML', 'CSS', 'JS', 'Python']
+    }
+
+print('Person information: ', person_info)
+
+name, age, is_men = 'HongGilDong', 25, True
+
+print(name, age, is_men)
