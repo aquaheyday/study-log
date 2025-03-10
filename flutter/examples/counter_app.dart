8f9308d6b13d6e6d8a536efde104c449d4cf5b12
ByoungJun Kim <aquaheyday@gmail.com>
2025-03-10 10:51:38 +0900

    Create counter_app.dart

---
+++ flutter/examples/counter_app.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Counter: $_counter', style: TextStyle(fontSize: 24)),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: Text('Increase'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
